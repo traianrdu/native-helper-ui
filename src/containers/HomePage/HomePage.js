92dc3cca6c31b27cdd33c384de868bf1603d7373
Traian <traian.plosca@gmail.com>
2022-10-09 00:52:05 +0300

    add: routes & navbar

---
+++ src/containers/HomePage/HomePage.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+/**
+ * USAGE OF CONTAINER EXAMPLE (NOT USED ANYMORE)
+ */
+
+/**
+ * Creates the page
+ * @returns default homepage
+ */
+export default function HomePage() {
+  return (
+    <div>
+        LOGO
+        {renderLogo()}
+    </div>
+    
+  )
+}
+
+/**
+ * Renders logo
+ * @returns logo
+ */
+function renderLogo() {
+    return (
+        <div>
+            log
+        </div>
+    )
+}
